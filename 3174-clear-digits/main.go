b5ca2914ec24771d22bc96977181fdc2248ed315
rahmandaniwinarta <daniwinarta31@gmail.com>
2025-02-10 15:43:29 +0700

    day 1

--- 3174-clear-digits/main.go
+++ 3174-clear-digits/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	//test
+	fmt.Println(ClearDigits("abc"))
+	fmt.Println(ClearDigits("cb34"))
+
+}
+
+func ClearDigits(s string) string {
+	slice := []rune{}
+	for _, huruf := range s {
+		if huruf >= '0' && huruf <= '9' {
+			if len(slice) > 0 {
+				slice = slice[:len(slice)-1]
+			}
+		} else {
+			slice = append(slice, huruf)
+		}
+	}
+	return string(slice)
 }
