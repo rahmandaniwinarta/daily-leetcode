2c9c73d4c112236b47891357e2917a0396158ef9
rahmandaniwinarta <daniwinarta31@gmail.com>
2025-02-13 08:50:19 +0700

    change to map

--- day3-max-sum-of-pair-with-equal-sum-of-digits/main.go
+++ day3-max-sum-of-pair-with-equal-sum-of-digits/main.go
@@ -5,22 +5,30 @@ import (
 )
 
 func main() {
-	nums := []int{1, 2, 2, 3, 4, 4, 4, 5}
+	// nums := []int{1, 2, 2, 3, 4, 4, 4, 5}
+	nums1 := []int{27, 18, 43, 36, 13, 7}
 
-	fmt.Println(maximumSum(nums))
+	// fmt.Println(maximumSum(nums))
+	fmt.Println(maximumSum(nums1))
+
 }
 
 func maximumSum(nums []int) int {
-	res := 0
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if sumIndex(nums[i]) == sumIndex(nums[j]) {
-				res = maxSum(res, nums[i]+nums[j])
-			}
+	res := -1
+
+	sumMap := make(map[int]int)
+
+	for _, num := range nums {
+		fmt.Println(sumMap)
+		sumIdx := sumIndex(num)
+		if sumMap[sumIdx] > 0 {
+			res = maxSum(res, sumMap[sumIdx]+num)
+		}
+		if sumMap[sumIdx] < num {
+			sumMap[sumIdx] = num
 		}
 	}
 	return res
-
 }
 
 func sumIndex(i int) int {
